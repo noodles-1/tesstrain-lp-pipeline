21ffd73de8eb383548662c5a3f7aa0f6c33df6ee
noodles-1 <adrianeroa081202@gmail.com>
2024-08-04 00:33:38 +0800

    line box fix

--- tesstrain/generate_line_box.py
+++ tesstrain/generate_line_box.py
@@ -1,65 +1,31 @@
-#!/usr/bin/env python3
-
-import argparse
-import io
+import pathlib
 import unicodedata
-
 from PIL import Image
 
-#
-# command line arguments
-#
-arg_parser = argparse.ArgumentParser(
+red = '\033[91m'
+reset = '\033[0m'
+
+def generate_line_box(gt_txt, image_path, output_path):
     """Creates tesseract box files for given (line) image text pairs"""
-)
-
-# Text ground truth
-arg_parser.add_argument(
-    '-t',
-    '--txt',
-    nargs='?',
-    metavar='TXT',
-    help='Line text (GT)',
-    required=True,
-)
-
-# Image file
-arg_parser.add_argument(
-    '-i',
-    '--image',
-    nargs='?',
-    metavar='IMAGE',
-    help='Image file',
-    required=True,
-)
-
-args = arg_parser.parse_args()
-
-#
-# main
-#
-
-# Get image size.
-width, height = Image.open(args.image).size
-
-# load gt
-with io.open(args.txt, 'r', encoding='utf-8') as f:
-    lines = f.read().strip().split('\n')
-    if len(lines) != 1:
-        raise ValueError(
-            'ERROR: %s: Ground truth text file should contain exactly one line, not %s'
-            % (args.txt, len(lines))
-        )
+    lines=pathlib.Path(gt_txt).read_text(encoding='utf-8').splitlines()
+    if  len(lines) != 1:
+        print(f"{red}Invalid gt_txt file: {gt_txt}{reset}")
+        return False
     line = unicodedata.normalize('NFC', lines[0].strip())
-
-if line:
-    for i in range(1, len(line)):
-        char = line[i]
-        prev_char = line[i - 1]
-        if unicodedata.combining(char):
-            print('%s 0 0 %d %d 0' % ((prev_char + char), width, height))
-        elif not unicodedata.combining(prev_char):
-            print('%s 0 0 %d %d 0' % (prev_char, width, height))
-    if not unicodedata.combining(line[-1]):
-        print('%s 0 0 %d %d 0' % (line[-1], width, height))
-    print('\t 0 0 %d %d 0' % (width, height))
+    if not line:
+        print(f"{red}Can not normalize line in gt_txt file: {blue}{gt_txt}{reset}")
+        return False
+    with Image.open(image_path) as image:
+        width, height = image.size
+    with open(output_path, 'w', encoding='utf-8') as out_file:
+        for i in range(1, len(line)):
+            char = line[i]
+            prev_char = line[i-1]
+            if unicodedata.combining(char):
+                out_file.write(f"{prev_char + char} 0 0 {width} {height} 0\n")
+            elif not unicodedata.combining(prev_char):
+                out_file.write(f"{prev_char} 0 0 {width} {height} 0\n")
+        if not unicodedata.combining(line[-1]):
+            out_file.write(f"{line[-1]} 0 0 {width} {height} 0\n")
+        out_file.write(f"\t 0 0 {width} {height} 0\n")
+    return True
